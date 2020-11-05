af64b64b9b9dca94143de8f7901d3be6120ece87
Tessa Cooper <55293851+tcooops@users.noreply.github.com>
2020-11-05 13:11:23 -0500

    added data miner

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
-(() => {
-    // start with a fetch all
-    
-    fetch('./DataSet.json') // this grabs the data from the source indicated in the brackets
-        .then(res => res.json()) // this translates the JSON to plain JS)
-        .then(data => { // then we get our data
-            // this is our data (dataSet.json)
-            // conbverted into a plain JavaScript object
-            handleDataSet(data);
-        })
-    .catch((error) => console.log(error));
+import { fetchData } from "./modules/DataMiner.js";
 
-    // this receives the data payload from our AJAX request, parses it (turns the returned JSON object back into a plain JavaScript object) and renders the data to our view (the markup in index.html)
+(() => {
+// this receives the data payload from our AJAX request, parses it (turns the returned JSON object back into a plain JavaScript object) and renders the data to our view (the markup in index.html)
+
     function handleDataSet(data) {
         let userSection = document.querySelector('.user-section'),
             userTemplate = document.querySelector('#user-template').content;  
             // note: you can't access the template directly, you have to dig in to grab the content
 
-        debugger;
+        //debugger;
 
         // loop through the JavaScript object and for each user, make a copy of the user template we find at the bottom of index.html, populate it with the user's data, and put that fresh copy in the users section in index.html
 
@@ -35,4 +27,8 @@
 
         console.log(data);
     }
+
+    // fetch data then do this with it (function above) then catch errors
+    fetchData('./DataSet.json').then(data => handleDataSet(data)).catch(err => console.log(err));
+
 })();
