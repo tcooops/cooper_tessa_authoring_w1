1a025efa6f0bdd9250eea55f803506792e8dd980
Tessa Cooper <55293851+tcooops@users.noreply.github.com>
2020-11-17 19:09:49 -0500

    added php as per WEEK6 class

---
+++ includes/functions.php
@@ -0,0 +1,16 @@
+<?php
+    // include the CONNECT.PHP file
+    include("connect.php");
+
+    $query = "SELECT * FROM tbl_profData";  // selects ALL from the table with our Professor Data
+
+    $runQuery = $pdo->query($query);  // this asks for the statement above (select all from prof data) and runs the query. PDO means portable data object.
+
+    $result = array();
+
+    while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+        $result[] = $row;
+    } // note: no semi-colon needed after the while statement. It is a body with curly braces so no need for a semi-colon. 
+
+    //return $result;
+    var_dump(json_encode($result));
